3ececf3f601857d65f692d3f499635bcd656e2c8
Sudhish Kumar <tssudhish@gmail.com>
2023-07-25 10:06:54 +0100

    still not working

--- src/tiles2.js
+++ src/tiles2.js
@@ -2,18 +2,22 @@
 const canvas = document.getElementById('tilesCanvas2');
 
 
+// create a drawing context
+const ctx = canvas.getContext('2d');
 
-// draw a point on the canvas when the user clicks on it
-canvas.addEventListener('click', (e) => {
-    const rect = canvas.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-    points.push({
-        x,
-        y
-    });
-    draw();
+// create  grid points of 5X10 (50 points) on the canvas and draw them as small circles of size 2
+const points = [];
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 10; j++) {
+        points.push({
+            x: i * 50,
+            y: j * 50
+        });
+    }
 }
-);
-
-
+ctx.fillStyle = 'black';
+for (let i = 0; i < points.length; i++) {
+    ctx.beginPath();
+    ctx.arc(points[i].x, points[i].y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+}
