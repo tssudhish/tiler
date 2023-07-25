69c62507149d75db1cfa3ca9704a832b43bfb005
Sudhish Kumar <tssudhish@gmail.com>
2023-07-25 14:30:17 +0100

    Some table creating in the index.html file 	modified:   src/index.html 	new file:   src/main.js 	new file:   src/temp.css

---
+++ src/main.js
@@ -0,0 +1,48 @@
+// make the button clickable and hello world appear
+
+function clickMe() {
+    myFunction();
+    newFunction();
+}
+
+
+function newFunction() {
+    console.log('hello world');
+    const grid = document.getElementById('grid');
+    const table = document.createElement('table');
+    table.style.border = '1px solid black';
+    table.style.borderCollapse = 'collapse';
+    for (let i = 0; i < 5; i++) {
+        const tr = document.createElement('tr');
+        for (let j = 0; j < 10; j++) {
+            const td = document.createElement('td');
+            td.style.border = '1px solid black';
+            td.style.width = '50px';
+            td.style.height = '50px';
+            tr.appendChild(td);
+            // some random text for each cell
+            const text = document.createTextNode(Math.floor(Math.random() * 100));
+            td.innerHTML = text.textContent;
+
+            
+        }
+        table.appendChild(tr);
+    }
+    grid.appendChild(table);
+}
+
+// function myFunction()  which on clicking create a hello world message
+function myFunction() {
+    // make the label appear and make it beautiful
+    const label = document.getElementById('lbl');
+    label.innerHTML = 'Hello World!';
+
+
+}
+
+// function changeColor() which on clicking change the color of the label
+function changeColor() {
+    // make the label appear and make it beautiful
+    const label = document.getElementById('lbl');
+    label.style.color = 'red';
+}
